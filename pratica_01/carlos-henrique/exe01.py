d6cfe76ca99f0a450dc1a58a3157dfade788c73d
marcolinoee <marcolinoee@google.com>
2026-05-15 21:10:50 -0300

    feat: atualização prática 02

---
+++ pratica_01/carlos-henrique/exe01.py
@@ -0,0 +1,5 @@
+nome = input("Qual seu nome?")
+idade = int(input("Qual é seu ano de nascimento?"))
+altura = float(input("Qual é sua altura?"))
+
+print(f"Olá {nome} Você tem {2026 - idade}) anos e mede {altura} de altura")
